dc0794e07bdbae78b8708c1d22204f0d462da2bf
Russell Richardson <Russell@KronosAD.com>
2021-11-10 00:04:31 -0500

    Core project update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ var app = express()
 
 app.listen(9000, () => {
     console.log("Redirector server is running on port 9000!");
+    console.log(`Using the following database credentials ${process.env.MONGO_USERNAME},
+     ${process.env.MONGO_PASSWORD}, ${process.env.MONGO_DATABASE}`);
 })
 
 app.get("/api/test", (req, res, next) => {
