6a1b0c0e9866a3f75df6f12c4a6527560779a50a
Russell Richardson <admin@russ.network>
2022-02-04 00:56:59 -0500

    It's v1 time!

--- src/api.js
+++ src/api.js
@@ -1,11 +1,19 @@
 let express = require('express');
+const { isValidObjectId } = require('mongoose');
 
 module.exports = function(db,  Redirect) {
 
     let router = express.Router();
 
+    let subfolder = process.env.HOST_UNDER || ""
+
+
+    let missingParamsError = { "error": "You are missing the required parameters.", "errorCode": "INVALID_PARAMETERS" };
+    let redirectNotFoundError = { "error": "The redirect with the provided key was not found", "errorCode": "REDIRECT_NOT_FOUND" };
+
+    
     var validateAuth = function(req, res, next) {
-        if(req.headers['apiKey'] != undefined || req.headers['apiKey'] !== process.env.API_KEY) {
+        if(req.headers['x-api-key'] === undefined || req.headers['x-api-key'] !== process.env.API_KEY) {
             res.status(401);
             res.json({ error: "You are not authorized to use this endpoint.", errorCode: "INVALID_AUTH" });
         } else {
@@ -13,8 +21,102 @@ module.exports = function(db,  Redirect) {
         }
     }
 
-    router.get('/api/', function(req, res) {
+    router.get(`${subfolder}/api/`, function(req, res) {
         res.json({"message": "You have successfully accessed Redirector!", "status": "OK"});
+    });
+
+    router.get(`${subfolder}/api/redirects/:key`, async function(req, res) {
+
+        if(req.params.key === undefined) {
+            res.status(400);
+            res.json(missingParamsError);
+            return;
+        }
+
+        let results = await Redirect.find({ redirect_from: req.params.key });
+
+        if(results.length == 0) {
+            res.status(404);
+            res.json(redirectNotFoundError);
+            return;
+        } else {
+            res.json(results[0]);
+        }
+
+    });
+
+    router.post(`${subfolder}/api/redirects/new`, validateAuth, async function(req, res) {
+        
+        if(req.body.redirect_from === undefined || req.body.redirect_to === undefined) {
+            res.status(400);
+            res.json(missingParamsError);
+            return;
+        }
+
+        let { redirect_from, redirect_to } = req.body;
+
+        // We don't necessarily care about case sensitivity, so everything will be stored lowercase
+        redirect_from = redirect_from.toLowerCase();
+        redirect_to = redirect_to.toLowerCase();
+
+        console.log(`New redirect request received with ${redirect_from} // ${redirect_to}`);
+
+        var existingResults = await Redirect.find({ redirect_to });
+
+        if(existingResults.length > 0) {
+            res.status(400);
+            res.json({ error: "That Redirection already exists!", errorCode: "REDIRECT_ALREADY_EXISTS" });
+            return;
+        }
+
+        var redirect = new Redirect({ redirect_from, redirect_to });
+        console.log(redirect);
+
+        await redirect.save();
+        
+        res.json({"message": "Success!", redirect});
+    });
+
+    router.patch(`${subfolder}/api/redirects/:key`, validateAuth, async function(req, res) {
+        if(req.params.key === undefined || req.body.redirect_to === undefined) {
+            res.status(400);
+            res.json(missingParamsError);
+            return;
+        }
+
+        let results = await Redirect.find({ redirect_from: req.params.key });
+
+        if(results.length == 0) {
+            res.status(404);
+            res.json(redirectNotFoundError);
+            return;
+        } else {
+            let redirect = results[0];
+            redirect.redirect_to = req.body.redirect_to;
+            await redirect.save();
+            res.json({"message": "Updated!", "redirect": redirect});
+        }
+    });
+
+    router.delete(`${subfolder}/api/redirects/:key`, validateAuth, async function(req, res) {
+        if(req.params.key === undefined) {
+            res.status(400);
+            res.json(missingParamsError);
+            return;
+        }
+
+        let results = await Redirect.find({ redirect_from: req.params.key });
+
+        if(results.length == 0) {
+            res.status(404);
+            res.json(redirectNotFoundError);
+            return;
+        } else {
+            let redirect = results[0];
+            await redirect.delete();
+            res.json({"message": "Redirect removed!"});
+        }
+
     });
     
     return router;
--- src/index.js
+++ src/index.js
@@ -4,20 +4,52 @@ let mongoose = require('mongoose');
 let app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
-app.listen(process.env.APP_PORT, async () => {
-    let { MONGO_USERNAME, MONGO_PASSWORD, MONGO_DATABASE } = process.env || {};
+app.listen((process.env.APP_PORT || 9500), async () => {
+    let { MONGO_USERNAME, MONGO_PASSWORD, MONGO_DATABASE, MONGO_HOST } = process.env || {};
 
     console.log(`Redirector server is running on port ${process.env.APP_PORT}!`);
-    await mongoose.connect(`mongodb://localhost:27017/${MONGO_DATABASE}`);
+    let connectionString = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:27017/${MONGO_DATABASE}?authSource=admin`;
+    console.log(`Using the following connection string: ${connectionString}`);
+    await mongoose.connect(connectionString);
 });
 
 let db = mongoose.connection;
 
-db.on('error', console.error.bind(console, "MongoDB connection error: "));
-db.on('connected', () => { console.log("Database successfully connected!" )});
+db.on('error', (error) => { 
+    console.error.bind(console, "MongoDB connection error: ") 
+    console.error(`MongoDB Connection Error: ${error}`)
+    process.exit(-1);
+});
 
-var Redirect = mongoose.model("Redirect", require('./RedirectSchema'));
+db.on('connected', () => { 
+    console.log("Database successfully connected!")
+});
+
+var Redirect = mongoose.model("Redirect", require('./RedirectSchema').redirectorSchema);
 
 app.use(require('./api')(db, Redirect));
+
+var subfolder = process.env.HOST_UNDER || ""
+console.log(`Hosting under ${subfolder || "root domain"}`)
+
+app.get(`${subfolder}/:key`, async function(req, res) {
+    if(req.params.key === undefined) {
+        res.status(400);
+        res.json({ "message": "Please provide a destination!" });
+        return;
+    }
+
+    let results = await Redirect.find({ redirect_from: req.params.key });
+
+    if(results.length == 0) {
+        res.status(404);
+        res.json({ "message": "That Redirect does not exist!"});
+        return;
+    }
+
+    res.redirect(results[0].redirect_to);
+
+});
